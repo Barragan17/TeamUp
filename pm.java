de03dcb5fcaa5bb8cfa9e9998cb343e2db3c8f85
linusrandu <linusrandu23@gmail.com>
2020-11-09 22:26:36 +0700

    Update pm.java

--- pm.java
+++ pm.java
@@ -6,39 +6,39 @@ public class pm {
     private User pm_email; 
     private User pm_mobile;
     private User pm_photo 
-    private ArrayList<User> Project;
-    private ArrayList<User> talentSuggUser;
+    private ArrayList<User> pm_project;
+    private ArrayList<User> pm_talentSugg;
 
     public ArrayList<User> haveProject() {
-        return this.Project;
+        return this.pm_project;
     }
 
     public ArrayList<User> haveTalentSugg() {
-        return this.talentSuggUser;
+        return this.pm_talentSugg;
     }
 
-    public void CreateProject(User Project) {
-        this.Project.add(Project);
+    public void CreateProject(User pm_project) {
+        this.Project.add(pm_project);
     }
 
-    public void EditProject(User Project) {
-        this.Project.edit(Project);
+    public void EditProject(User pm_project) {
+        this.Project.edit(pm_project);
     }
 
-    public void DeleteProject(User Project) {
-        this.Project.delete(Project);
+    public void DeleteProject(User pm_project) {
+        this.Project.delete(pm_project);
     }
 
-    public void CreateProject(User Project) {
-        this.Project.add(Project);
+    public void CreateProject(User pm_project) {
+        this.Project.add(pm_project);
     }
 
-    public void LeftSwipe(User talentSuggUser) {
-        this.Project.delete(talentSuggUser);
+    public void LeftSwipe(User pm_talentSugg) {
+        this.Project.delete(pm_talentSugg);
     }
 
-    public void RightSwipe(User talentSuggUser) {
-        this.Project.add(talentSuggUser);
+    public void RightSwipe(User pm_talentSugg) {
+        this.Project.add(pm_talentSugg);
     }
     
 }
